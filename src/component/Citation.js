e712cca90684b3b02b5409259794658ef5c8db82
Steven Robertson <steven.robertson@worley.com>
2023-02-27 18:22:04 +0000

    Removing redundant plugin

--- src/component/Citation.js
+++ src/component/Citation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import '@citation-js/plugin-bibtex';
-import '@citation-js/plugin-ris';
 import '@citation-js/plugin-csl';
 import {Cite, plugins} from "@citation-js/core";
 
